66b15468f92a24282fdb9caad2e383d7319f2e97
dahwin <60541501+dahwin@users.noreply.github.com>
2022-08-14 20:15:41 -0700

    Implement getComputerChoice()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const move = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    // generates a number from the range of 0 through 2 (inclusive)
+    let randomNumber = Math.floor(Math.random() * 3)
+    return move[randomNumber];
+}
